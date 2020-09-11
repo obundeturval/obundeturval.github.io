8f5c3aeb1668e08e360917a07f1e17dfbd28a1bd
Jan Novotny <obundeturvalblog@gmail.com>
2020-09-11 22:16:02 +0200

    Nikola auto commit.
    Nikola version: 8.0.4

--- conf.py
+++ conf.py
@@ -155,7 +155,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootblog4"
+THEME = "bootstrap"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
