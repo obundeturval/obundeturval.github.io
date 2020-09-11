f8e814f0055dcd17fb8661294caa5345bf79e76b
Jan Novotny <obundeturvalblog@gmail.com>
2020-09-11 22:22:42 +0200

    Nikola auto commit.
    Nikola version: 8.0.4

--- conf.py
+++ conf.py
@@ -155,7 +155,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+THEME = "detox"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
